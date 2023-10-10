6a57ecfe2ba232d25b2856faed666e8d563d650c
João Rodrigues <joao.pdsrodrigues@gmail.com>
2023-10-10 21:39:29 +0100

    spring initializr project

---
+++ study-capsule/src/main/java/com/sirba/studycapsule/StudyCapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.sirba.studycapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCapsuleApplication.class, args);
+	}
+
+}
---
+++ study-capsule/src/test/java/com/sirba/studycapsule/StudyCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirba.studycapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
